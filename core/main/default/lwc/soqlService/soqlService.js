754126c38b265424f7932ccf7b47ad9c768950ad
Suraj Pillai <85.suraj@gmail.com>
2022-04-11 00:20:45 -0400

    WIP: Soql params

--- core/main/default/lwc/soqlService/soqlService.js
+++ core/main/default/lwc/soqlService/soqlService.js
@@ -1,6 +1,12 @@
 import executeSoql from "@salesforce/apex/DynamicSOQLDMLController.executeSoqlQuery";
+import executeSoqlWithParams from "@salesforce/apex/DynamicSOQLDMLController.executeSoqlQueryWithParams";
 
-export default async function soql(query) {
-  let results = await executeSoql({ query });
+export default async function soql(query, params) {
+  let results;
+  if (params) {
+    results = await executeSoqlWithParams({ query, params });
+  } else {
+    results = await executeSoql({ query });
+  }
   return results;
 }
