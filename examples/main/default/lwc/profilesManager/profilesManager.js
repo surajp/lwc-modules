d8893ea4d10eaa026ce8f95a805a1be25aaf6e2e
Suraj Pillai <suraj.pillai@vertexcs.com>
2022-05-03 23:24:59 -0400

    Parse errors and render in datatable

--- examples/main/default/lwc/profilesManager/profilesManager.js
+++ examples/main/default/lwc/profilesManager/profilesManager.js
@@ -14,6 +14,7 @@ export default class ProfilesManager extends LightningElement {
   _pageNum = 0;
   _pageSize = 10;
   _permLabelsMap;
+  tableErrors;
   _query =
     "Select FIELDS(STANDARD) from Profile where Id in (Select ProfileId from PermissionSet where IsCustom=true ) and UserType='Standard' limit 15";
 
@@ -94,7 +95,7 @@ export default class ProfilesManager extends LightningElement {
   }
 
   async _fetchProfilesData() {
-    this.columns = [{ label: "Name", fieldName: "Name", initialWidth: 200 }];
+    this.columns = [{ label: "Name", fieldName: "Label", initialWidth: 400 }];
     try {
       this.profileData = await soql(this._query);
       this._createMapOfProfileIdByName();
@@ -111,19 +112,18 @@ export default class ProfilesManager extends LightningElement {
   handleSave(evt) {
     const profiles = this._convertDraftValuesToProfileData(evt.detail.draftValues);
     const profilesToUpdate = this._convertToSubRequests(profiles);
-    this._updateProfiles(profilesToUpdate);
+    this._updateProfiles(profilesToUpdate, profiles);
   }
 
   _convertDraftValuesToProfileData(draftValues) {
     const retVal = {};
-    draftValues.forEach((d) => {
-      const perm = d.Name;
-      Object.keys(d).forEach((k) => {
-        if (k === "Name") return;
-        const id = this.profileIdMap[k];
+    draftValues.forEach((row) => {
+      const perm = row.Name;
+      Object.keys(row).forEach((colName) => {
+        if (colName === "Name") return;
+        const id = this.profileIdMap[colName];
         if (!retVal[id]) retVal[id] = {};
-        let obj = retVal[id];
-        obj["Permissions" + perm] = d[k];
+        retVal[id]["Permissions" + perm] = row[colName];
       });
     });
     return retVal;
@@ -141,11 +141,11 @@ export default class ProfilesManager extends LightningElement {
     return retObj;
   }
 
-  async _updateProfiles(profilesToUpdate) {
+  async _updateProfiles(profilesToUpdate, profiles) {
     try {
       const resp = await sfapi("/composite/batch", "POST", {}, JSON.stringify(profilesToUpdate));
       if (resp.hasErrors) {
-        let errors = this._reduceCompositeErrors(resp.results);
+        let errors = this._reduceCompositeErrors(resp.results, profiles);
         this.dispatchEvent(new ShowToastEvent({ mode: "sticky", variant: "error", message: errors }));
       } else {
         this.dispatchEvent(new ShowToastEvent({ variant: "success", message: "Profiles updated" }));
@@ -159,13 +159,37 @@ export default class ProfilesManager extends LightningElement {
     }
   }
 
-  _reduceCompositeErrors(results) {
+  _reduceCompositeErrors(results, profiles) {
+    console.log(">>> results", results); //inspect result shape
     let errors = results
       .reduce(
         (obj, curr) => (curr.statusCode < 399 && obj) || [...obj, ...curr.result.map((r) => r.message)] || obj,
         []
       )
-      .join("\n");
+      .join("<br>");
+    let rowErrors = {},
+      tableErrors = { messages: [] };
+    let profileIds = Object.keys(profiles);
+    results.forEach((res, index) => {
+      if (res.statusCode < 399) return;
+      let profileId = profileIds[index];
+      let errColName = this.profileData.filter((p) => p.Id === profileId)[0].Name;
+      res.result.forEach((singleError) => {
+        let matches = singleError.message.match(/permission(\(s\))?: (.*)$/);
+        if (matches && matches.length > 2) {
+          let rowKey = matches[2];
+          if (!rowErrors[rowKey]) rowErrors[rowKey] = { messages: [], fieldNames: [] };
+          rowErrors[rowKey].messages.push(singleError.message);
+          rowErrors[rowKey].fieldNames.push(errColName);
+        }
+        tableErrors.messages.push(singleError.message);
+      });
+    });
+    tableErrors.title = `You have ${tableErrors.messages.length} errors`;
+    this.tableErrors = {
+      rows: rowErrors,
+      table: tableErrors
+    };
     return errors;
   }
 
